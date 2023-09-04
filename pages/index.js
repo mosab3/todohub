c7489bca0a1aa6618e649226f0c2a802192f1206
Musab Mohamed <mosab98@live.com>
2023-09-04 21:38:55 +0300

    add container component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
+import { Container } from '@/components/main'
 // import styles from '@/styles/Home.module.css'
 
 
 export default function Home() {
   return (
     <>
+      <Container>
+        <h1>Hi</h1>
+      </Container>
     </>
   )
 }
