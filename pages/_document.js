000688845e038894f0fa043a8088b1fe1bd78404
Musab Mohamed <mosab98@live.com>
2023-09-08 15:20:12 +0300

    fix navbar

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ import { Navbar } from '@/components/main'
 export default function Document() {
   return (
     <Html lang="en">
-      <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.ADSENSE_PUB}`} crossorigin="anonymous"></script>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Yet Another TODO List</title>
+      <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.ADSENSE_PUB}`} crossOrigin="anonymous"></script>
       <Head />
       <body>
         <Navbar />
