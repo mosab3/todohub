d4386c84a5c47298913f6de5648a7323a3bd5ff7
Musab Mohamed <mosab98@live.com>
2023-09-16 14:09:37 +0300

    add id to items object

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Container, Card, List, Navbar } from '@/components/main'
 import { Items } from '@/components/list'
 import { useState, useEffect } from 'react'
-
+import { hashString } from 'react-hash-string'
 
 export default function Home() {
 
@@ -15,7 +15,7 @@ export default function Home() {
     }
   }, [])
 
-  const handelEnter = (event) => {
+  const handleEnter = (event) => {
     let value = message.trim()
     let isObjPresent = todo.some(obj => {
       return obj.text === value
@@ -23,7 +23,7 @@ export default function Home() {
     if (event.key == 'Enter' || event.type == 'click') {
       if (value != "" && isObjPresent == false) {
         const updatedTodo = [
-          {text: value, checked: false},
+          {id: hashString(value), text: value, checked: false},
           ...todo,
         ]
         setTodo(updatedTodo)
@@ -33,25 +33,23 @@ export default function Home() {
     }
   }
 
-  const handelCheck = (event) => {
-    let checked = event.target.checked
-    let value = event.target.value
-    const updatedTodo = (prevTodo, checked, value) => {
+  const handleCheck = (checked, id) => {
+    const updatedTodo = (prevTodo, checked, id) => {
       return prevTodo.map((obj) => {
-        if (obj.text === value) {
+        if (obj.id === id) {
           return { ...obj, checked };
         }
         return obj;
       });
     }
-    const reserveTodo = updatedTodo(todo, checked, value)
+    const reserveTodo = updatedTodo(todo, checked, id)
     setTodo(reserveTodo);
     localStorage.setItem('list', JSON.stringify(reserveTodo))
 
   }
 
-  const handelDelete = (textToDelete) => {
-    const updatedTodo = todo.filter((obj) => obj.text !== textToDelete);
+  const handleDelete = (id) => {
+    const updatedTodo = todo.filter((obj) => obj.id !== id);
     setTodo(updatedTodo);
     localStorage.setItem('list', JSON.stringify(updatedTodo))
   }
@@ -64,23 +62,23 @@ export default function Home() {
           <List>
             <div className='mb-3'>
               <div className='input-group flex-nowrap'>
-                <span className='input-group-text'><i className="bi bi-plus-lg" onClick={handelEnter}></i></span>
-                <input dir='auto' value={message} onChange={event => setMessage(event.target.value)} className='form-control' placeholder='fix that bug...' onKeyDown={handelEnter}/>
+                <span className='input-group-text'><i className="bi bi-plus-lg" onClick={handleEnter} /></span>
+                <input dir='auto' value={message} onChange={event => setMessage(event.target.value)} className='form-control' placeholder='fix that bug...' onKeyDown={handleEnter}/>
               </div>
             </div>
             <Items
               arr={todo}
               isChecked={false}
-              onCheck={handelCheck}
-              onDelete={handelDelete}
+              onCheck={handleCheck}
+              onDelete={handleDelete}
               isDivSpace={true}
             />
             
             <Items
               arr={todo}
               isChecked={true}
-              onCheck={handelCheck}
-              onDelete={handelDelete}
+              onCheck={handleCheck}
+              onDelete={handleDelete}
               isDivSpace={false}
               isGrayedOut={true}
             />
