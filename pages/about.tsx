297fb7bb37037bf4e9fc08f4e13dcdc4c42cbaf2
Musab Mohamed <mosab98@live.com>
2024-10-12 10:38:00 +0300

    fix build issue

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,22 @@
-import { Container, Card, Navbar } from '@/components/main'
+import { Container, Card, Navbar, Wrapper } from '@/components/main'
 
 
 export default function About() {
     return (
         <>
-            <Navbar />
-            <Container>
-                <Card>
-                    <h1 className="display-4">About Me</h1>
-                    <hr/>
-                    <br/>
-                    <p>
-                        Just some random software engineer.
-                    </p>
-                </Card>
-            </Container>
+            <Wrapper>
+                <Navbar />
+                <Container>
+                    <Card>
+                        <h1 className="display-4">About Me</h1>
+                        <hr/>
+                        <br/>
+                        <p>
+                            Just some random software engineer.
+                        </p>
+                    </Card>
+                </Container>
+            </Wrapper>
         </>
     )
 
