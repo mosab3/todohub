c05b7eb04dc7e90591ed4b4970e83abd94f7c7ad
Musab Mohamed <mosab98@live.com>
2025-01-02 15:44:13 +0300

    update packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@/styles/Home.module.css'
-import { useEffect } from "react";
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
 
   return <Component {...pageProps} />
 }
